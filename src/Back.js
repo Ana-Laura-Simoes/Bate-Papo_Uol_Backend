bcfb5dad28536df41c476f2826a6b51fffd3518d
Ana Simoes <ana.simoes-mato@unesp.br>
2021-06-10 23:49:35 -0300

    entrar na sala,lista de participantes e enviar mensagem

---
+++ src/Back.js
@@ -0,0 +1,51 @@
+import express from 'express';
+import cors from 'cors';
+import dayjs from 'dayjs';
+
+const app = express();
+app.use(express.json());
+var corsOptions = {
+    origin: '*',
+}
+app.use(cors(corsOptions));
+
+//const user={name: 'João', lastStatus: 12313123};
+let participants=[];
+let messages=[];
+
+app.post('/participants', (req,res) => {
+    const participant = req.body.name;
+
+    if(participant && !participants.some(({name}) => name === participant)) {
+        participants.push({name: participant,lastStatus: Date.now()})
+        messages.push({from: participant, to: 'Todos', text: 'entra na sala', type: 'status', time: dayjs().format('HH:mm:ss')})
+        //console.log(messages);
+        return res.sendStatus(200)
+    }
+    res.sendStatus(400)
+});
+
+
+app.get('/participants', (req,res) => {
+res.send(participants);
+});
+
+
+app.post('/messages', (req,res)=>{
+const message=req.body;
+const from = req.header("User");
+
+if(message.to.length >0 && message.text.length>0 &&(message.type==='message' || message.type==='private_message') && participants.some(({name}) => name === from))
+{messages.push({from,...message,time: dayjs().format('HH:mm:ss')});
+console.log(messages);
+return res.sendStatus(200);
+}
+
+else res.sendStatus(400);
+});
+
+
+
+
+
+app.listen(4000,()=>{console.log("rodando servidor")});
---
+++ src/app.js
@@ -0,0 +1,50 @@
+import express from 'express';
+import cors from 'cors';
+import dayjs from 'dayjs';
+
+const app = express();
+app.use(express.json());
+var corsOptions = {
+    origin: '*',
+}
+app.use(cors(corsOptions));
+
+//const user={name: 'João', lastStatus: 12313123};
+let participants=[];
+let messages=[];
+
+app.post('/participants', (req,res) => {
+    const participant = req.body.name;
+
+    if(participant && !participants.some(({name}) => name === participant)) {
+        participants.push({name: participant,lastStatus: Date.now()})
+        messages.push({from: participant, to: 'Todos', text: 'entra na sala', type: 'status', time: dayjs().format('HH:mm:ss')})
+        //console.log(messages);
+        return res.sendStatus(200)
+    }
+    res.sendStatus(400)
+});
+
+
+app.get('/participants', (req,res) => {
+res.send(participants);
+});
+
+
+app.post('/messages', (req,res)=>{
+console.log(req.body);
+
+//const from = req.header("User");
+
+if(message.to.length >0 && message.text.length>0)
+//messages.push({...message, from});
+console.log(messages);
+
+
+});
+
+
+
+
+
+app.listen(4000,()=>{console.log("rodando servidor")});
